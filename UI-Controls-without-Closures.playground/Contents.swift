94cf20f431e42cbdb2f45180d2adaaf9d64595b9
rsgc-elder-a <aelder@rsgc.on.ca>
2016-04-25 21:03:22 -0400

    Not letting me update

--- UI-Controls-without-Closures.playground/Contents.swift
+++ UI-Controls-without-Closures.playground/Contents.swift
@@ -7,10 +7,6 @@ import XCPlayground
 // is a sub-class of UIViewController
 class ViewController : UIViewController {
     
-    // Create a method that runs when the switch is toggled
-    func bing() {
-        print("Bing!")
-    }
     
     
     // This method runs when the superview loads
@@ -19,69 +15,92 @@ class ViewController : UIViewController {
         // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
         // own version of viewDidLoad()
         super.viewDidLoad()
-
-        // Make the view's background be gray
-        view.backgroundColor = .grayColor()
         
-        /* 
+        // Make the view's background be gray
+        view.backgroundColor = .cyanColor()
+        
+        /*
          * Create the switch object
          */
-        let mySwitch : UISwitch = UISwitch()
         
-        // Required to autolayout this label
-        mySwitch.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Make the "bing" method fire when the switch is used
-        mySwitch.addTarget(self, action: #selector(ViewController.bing), forControlEvents: UIControlEvents.TouchUpInside)
-
-        // Add the switch to the superview
-        view.addSubview(mySwitch)
         
         /*
          * Create and position the label
          */
-        let myLabel = UILabel()
+        let title = UILabel()
+        let amount = UILabel()
+        let thePercentge = UILabel()
+        let total = UILabel()
+        let userAmount = UITextField()
+        let calc = UIButton()
         
+        //    let myLabel2 = UILabel()
         // Set the label text and appearance
-        myLabel.text = "Toggle me!"
-        myLabel.font = UIFont.boldSystemFontOfSize(36)
+        title.text = "Tip Calculator"
+        title.font = UIFont.boldSystemFontOfSize(36)
+        
+        amount.text = "Amount"
+        amount.font = UIFont.systemFontOfSize(24)
+        
+        thePercentge.text = "Percentage Tip"
+        thePercentge.font = UIFont.systemFontOfSize(24)
+        
+        
+        total.text = "Total"
+        total.font = UIFont.systemFontOfSize(24)
+        
+        //calc.text = ""
+        
+        
         
         // Required to autolayout this label
-        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        amount.translatesAutoresizingMaskIntoConstraints = false
+        
+        thePercentge.translatesAutoresizingMaskIntoConstraints = false
+        
+        total.translatesAutoresizingMaskIntoConstraints = false
+        
+        userAmount.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         
         // Add the label to the superview
-        view.addSubview(myLabel)
+        view.addSubview(title)
+        view.addSubview(amount)
+        view.addSubview(thePercentge)
+        view.addSubview(total)
         
         /*
          * Layout all the interface elements
          */
-
+        
         view.translatesAutoresizingMaskIntoConstraints = false
         
         // Create an empty list of constraints
         var allConstraints = [NSLayoutConstraint]()
         
         // Create a dictionary of views that will be used in the layout constraints defined below
-        let viewsDictionary : [String : AnyObject] = ["switch": mySwitch,
-                     "label": myLabel]
+        let viewsDictionary : [String : AnyObject] = ["title": title, "amount": amount, "percentage": thePercentge, "total": total, "useramount":userAmount]
         
         // Define the vertical constraints
         let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[label][switch]",
+            "V:[title][amount][percentage][total]",
             options: [],
             metrics: nil,
             views: viewsDictionary)
-
+        
         // Add the vertical constraints to the list of constraints
         allConstraints += verticalConstraints
-
+        
         // Define the horizontal constraints
         let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[label][switch]",
+            "H:[title]",
             options: [],
             metrics: nil,
             views: viewsDictionary)
-
+        
         // Add the vertical constraints to the list of constraints
         allConstraints += horizontalConstraints
         
